20b91b9be4ad81f13ba205fd49144bb3d6343445
Mateus Paixão <mateus.borges.paixao@gmail.com>
2019-07-31 02:22:14 -0300

    Updates

---
+++ precache-manifest.f47e713b9e708bb4b47dcd3280e97b90.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d61ecd3efed26e1693627233672a91da",
+    "url": "/Pipefy-Clone/index.html"
+  },
+  {
+    "revision": "a3205595bc1a69adae8f",
+    "url": "/Pipefy-Clone/static/js/2.d993bac6.chunk.js"
+  },
+  {
+    "revision": "dfc379dafa16506f3751",
+    "url": "/Pipefy-Clone/static/js/main.d6a696a6.chunk.js"
+  },
+  {
+    "revision": "9d2d5b0c1ca6d49c4e10",
+    "url": "/Pipefy-Clone/static/js/runtime~main.947584a2.js"
+  }
+]);
